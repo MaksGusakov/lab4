b00a29bdb433a07d8f6094926d50406216c09dab
MaksGusakov <gusakov77list.ru@gmail.com>
2025-05-03 21:19:33 +0300

    Update trpo.java

--- trpo.java
+++ trpo.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         String surname = "Гусаков";
-        String subject = "ТРПО";
+        String subject = "ТРП";
         System.out.println(surname);
         System.out.println(subject);
     }
