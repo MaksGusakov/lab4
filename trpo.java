30eb8d1468c58e0947f90eb817a49a8e3c1c93e5
MaksGusakov <gusakov77list.ru@gmail.com>
2025-05-03 21:01:55 +0300

    Create trpo.java

---
+++ trpo.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String surname = "Гусаков";
+        String subject = "ТРПО";
+        System.out.println(surname);
+        System.out.println(subject);
+    }
+}
